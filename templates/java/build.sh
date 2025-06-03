07b16eb640644d3ffdad6bc9b9767a26eff257b3
Adrien Wehrung <a.wehrung@finatix.de>
2025-06-03 11:32:09 +0200

    improve build scripts, update minor dependencies

--- templates/java/build.sh
+++ templates/java/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# make sure this includes your registry address if you want to upload the solution
-IMAGE_NAME="my-java-competitor:0.0.2"
+if [ -z $IMAGE_NAME ]; then
+  echo "Use the IMAGE_NAME variable to tag your container. Make sure to include your registry address if you want to upload the solution"
+  echo "Example: "
+  echo "  IMAGE_NAME=\"my.company.com/my-java-competitor:0.0.2\" ./build.sh"
+  exit 1
+fi
 
 mvn -U clean install -DskipTests
 docker build --no-cache -t ${IMAGE_NAME} .
--- templates/python/build.sh
+++ templates/python/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# make sure this includes your registry address if you want to upload the solution
-IMAGE_NAME="my-python-competitor:0.0.1"
+if [ -z $IMAGE_NAME ]; then
+  echo "Use the IMAGE_NAME variable to tag your container. Make sure to include your registry address if you want to upload the solution"
+  echo "Example: "
+  echo "  IMAGE_NAME=\"my.company.com/my-python-competitor:0.0.1\" ./build.sh"
+  exit 1
+fi
 
 docker build --no-cache -t ${IMAGE_NAME} .
 docker push ${IMAGE_NAME}
