47ae5a7c936fd07e511e207cf6ca77bbaa811638
Adrien Wehrung <a.wehrung@finatix.de>
2025-06-04 08:51:07 +0200

    fix tabulation

--- templates/java/test.sh
+++ templates/java/test.sh
@@ -4,7 +4,7 @@ if [ $# -eq 0 ]; then
   echo 'Missing arguments. Example usages:'
   echo 'Game 1: ./test.sh "[B,C,C]" "[C,B,C]"'
   echo 'Game 2: ./test.sh 30/2/S 30/1/P 20/0/P'
-	exit 1
+  exit 1
 fi
 
 mvn install
